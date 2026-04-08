a50beb4a0f5e641a92649371148867a2ed187e42
Mikkel Agerlin <ma@complycloud.com>
2026-04-08 08:57:51 +0200

    fix: re-add lualine

---
+++ nvim/lua/plugins/lualine.lua
@@ -0,0 +1,28 @@
+return {
+  "nvim-lualine/lualine.nvim",
+  dependencies = { "nvim-tree/nvim-web-devicons" },
+  config = function()
+    require("lualine").setup({
+      options = {
+        theme = "catppuccin-mocha",
+        section_separators = "",
+        component_separators = "",
+      },
+      sections = {
+        lualine_a = { "mode" },
+        lualine_b = { "branch" },
+        lualine_c = { "filename" },
+        lualine_x = { "fileformat", "filetype" },
+        lualine_z = { "location" },
+      },
+      inactive_sections = {
+        lualine_a = {},
+        lualine_b = {},
+        lualine_c = { "filename" },
+        lualine_x = { "location" },
+        lualine_y = {},
+        lualine_z = {},
+      },
+    })
+  end,
+}
